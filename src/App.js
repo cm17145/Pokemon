1724ab2bb79881ccb06d6aa66bccfab68fd832ab
cm17145 <cm17145@ues.edu.sv>
2023-10-26 19:00:29 -0600

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import Home from "../src/Pages/Home";
+import Courses from "../src/Pages/Courses";
+import Live from "../src/Pages/Live";
+import Contact from "../src/Pages/Contact";
+import Contador from "../src/Pages/Contador"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <ul>
+          <Link to="/" class="list">
+            Home
+          </Link>
+          <Link to="/course" class="list">
+            Courses
+          </Link>
+          <Link to="/live" class="list">
+            Live course
+          </Link>
+          <Link to="/contact" class="list">
+            Contact
+          </Link>
+          <Link to="/contador" class="list">
+            Contador
+          </Link>
+        </ul>
+      </nav>
+
+      {/* Defining routes path and rendering components as element */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+        <Route path="/live" element={<Live />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contador" element={<Contador />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App
